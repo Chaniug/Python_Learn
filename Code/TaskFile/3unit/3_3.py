b4e680d580d1fe171d0dcafca07cf467abab61f4
Chaniug <chaniug@vip.qq.com>
2023-10-17 16:31:36 +0800

    新代码增加

---
+++ Code/TaskFile/3unit/3_3.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author: chaniug
+# @Email: chaniug@vip.qq.com
+# @Time: 2023/10/17 0017 下午 16:27
+travels = ['bejing','wuhan','sichuan','guizhou','chongqing']
+for i in travels:
+    print(f"我想去{i}旅游")
