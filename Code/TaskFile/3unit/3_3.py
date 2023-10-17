9635651af7b4b4d89449a0c827dee0572c353fb6
Chaniug <chaniug@vip.qq.com>
2023-10-17 17:37:44 +0800

    新代码增加

--- Code/TaskFile/3unit/3_3.py
+++ Code/TaskFile/3unit/3_3.py
@@ -3,6 +3,30 @@
 # Author: chaniug
 # @Email: chaniug@vip.qq.com
 # @Time: 2023/10/17 0017 下午 16:27
-travels = ['bejing','wuhan','sichuan','guizhou','chongqing']
-for i in travels:
-    print(f"我想去{i}旅游")
+# travels = ['bejing','wuhan','sichuan','guizhou','chongqing']
+# # for i in travels:
+# #     print(f"我想去{i}旅游")
+# # sorted(travels)
+# # print(travels)
+# # sorted(travels,reverse=True)
+# # print(travels)
+# # travels.sort()
+# # print(travels)
+# travels.sort(reverse=True)
+# print(travels)
+# print(f"一共有{len(travels)}个可以旅游的地方")
+city = ['bejing','wuhan','sichuan','guizhou','chongqing','shanghai']
+rive = ['yellorive','changjiang','dongtinghu','haihei','nanhai']
+country = ['china','usa','japan','france','germany','russia']
+city.sort()
+print(city)
+sorted(city,reverse=True)
+print(city)
+rive.sort()
+print(rive)
+country.sort(reverse=True)
+print(country)
+travels = city + rive + country
+print(travels)
+country.reverse()
+print(country)
