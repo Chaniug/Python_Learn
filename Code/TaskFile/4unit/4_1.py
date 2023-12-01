c7b5d84cacac9c29924ba4c1d60e07fdcd4becf2
Chaniug <chaniug@vip.qq.com>
2023-12-01 15:06:43 +0800

    new code

---
+++ Code/TaskFile/4unit/4_1.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author: chaniug
+# @Email: chaniug@vip.qq.com
+# @Time: 2023/12/1 0001 下午 14:57
+#
+# 通过for循环遍历列表
+magics =['jeasm','yuki','mike','john','wohn']
+for magices in magics:
+    print(f'{magices.title()}是最好的魔术师！')
+
