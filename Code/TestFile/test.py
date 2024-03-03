000d6a86282160ff71157e0865a19c61ee13510b
Chaniug <chaniug@vip.qq.com>
2024-03-03 10:24:36 +0800

    NEW file value

--- Code/TestFile/test.py
+++ Code/TestFile/test.py
@@ -1,5 +1,9 @@
 print("你好，世界 ")
-for i in range(1, 10):
-    for j in range(1, i + 1):
-        print(f"{i} * {j} = {i * j}", end="\t")
-    print()
+# for i in range(1, 10):
+#     for j in range(1, i + 1):
+#         print(f"{i} * {j} = {i * j}", end="\t")
+#     print()
+arr = [1, 2, 3, 4, 5]
+
+double_arr = [arr[i] * 2 for i in range(len(arr))]
+print(double_arr)
