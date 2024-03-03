4b1a27c1e729c1dc3391afda54a65ef223b110b9
Chaniug <chaniug@vip.qq.com>
2024-03-03 10:28:55 +0800

    NEW file value

--- Code/TestFile/test.py
+++ Code/TestFile/test.py
@@ -1,9 +1,10 @@
+from typing import List
+
 print("你好，世界 ")
 # for i in range(1, 10):
 #     for j in range(1, i + 1):
 #         print(f"{i} * {j} = {i * j}", end="\t")
 #     print()
 arr = [1, 2, 3, 4, 5]
-
-double_arr = [arr[i] * 2 for i in range(len(arr))]
+double_arr: list[int] = [arr[i] * 2 for i in range(len(arr))]
 print(double_arr)
